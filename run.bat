c1bdf716c7990a15ec137971964d5f5e94650600
damienyf <damienyf@gmail.com>
2018-04-12 04:49:37 -0500

    initial commit

---
+++ run.bat
@@ -0,0 +1,40 @@
+﻿REM start python indeed-scrape.py "Software Developer"
+REM start python indeed-scrape.py "Portfolio Analyst"
+REM start python indeed-scrape.py "Strategic Sourcing Manager"
+REM start python indeed-scrape.py "Consultant"
+REM start python indeed-scrape.py "Data Scientist"
+REM start python indeed-scrape.py "Data Analyst"
+REM start python indeed-scrape.py "Project Manager"
+REM start python indeed-scrape.py "Java Developer"
+REM start python indeed-scrape.py "Quantitative Analyst"
+REM start python indeed-scrape.py "Program Manager"
+REM start python indeed-scrape.py "Investment Banking Analyst"
+REM start python indeed-scrape.py "Quantitative Trader"
+REM start python indeed-scrape.py "Global Markets Analyst"
+REM start python indeed-scrape.py "Data Engineer"
+REM start python indeed-scrape.py "Business Intelligence Engineer"
+REM start python indeed-scrape.py "Pricing Analyst"
+REM start python indeed-scrape.py "Operations Analyst"
+REM start python indeed-scrape.py "Risk Management"
+REM start python indeed-scrape.py "Media Analyst"
+REM start python indeed-scrape.py "Business Analyst"
+
+
+
+
+
+
+
+
+
+
+start python indeed-scrape.py "Project Manager"
+start python indeed-scrape.py "Consultant"
+
+
+
+
+
+
+
+
